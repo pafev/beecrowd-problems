edc628238c200cb8607a79445e1b1fd58d083f75
pafev <paulofvilarimda@gmail.com>
2025-07-17 17:18:23 -0300

    feat: update template

--- template-cpp.cpp
+++ template-cpp.cpp
@@ -3,13 +3,21 @@
 using namespace std;
 
 #define endl '\n'
+#define sys                                                                    \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);
 
 const int INF = 0x3f3f3f3f;
 const long long LINF = 0x3f3f3f3f3f3f3f3fll;
 
+typedef long long ll;
+
+void solve() {}
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
+  sys;
+
+  solve();
 
   return 0;
 }
